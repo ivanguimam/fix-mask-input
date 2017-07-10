5e15d2323a4ecb75d0d0a511383e2ae87470366f
Ivan Guimarães Monte <ivg.monte@gmail.com>
2017-07-10 15:55:16 -0300

    Melhorando diretiva

--- fix-mask-input.js
+++ fix-mask-input.js
@@ -3,15 +3,41 @@ angular.module('fixMaskInput', [])
 		return {
 			restrict: 'A',
 			link: function(scope, element, attr) {
-				element[0].addEventListener('keydown',
-					function(ev) {
-						if (ev.key != 8 && ev.key != 'Backspace') {
-							setTimeout(function() {
-								ev.target.selectionStart = ev.target.value.length
-								ev.target.selectionEnd = ev.target.value.length
-							})
-						}
-					})
+				addFocusEvent(element[0])
+				addClickEvent(element[0])
+				addKeyDownEvent(element[0])
 			}
+		}
+
+		function addKeyDownEvent(element) {
+			element.addEventListener('keydown',
+				function(ev) {
+					if (ev.key != 8 && ev.key != 'Backspace') {
+						setTimeout(function() {
+							ev.target.selectionStart = ev.target.value.length
+							ev.target.selectionEnd = ev.target.value.length
+						})
+					}
+				})
+		}
+
+		function addClickEvent(element) {
+			element.addEventListener('click',
+				function(ev) {
+					setTimeout(function() {
+						ev.target.selectionStart = ev.target.value.length
+						ev.target.selectionEnd = ev.target.value.length
+					})
+				})
+		}
+
+		function addFocusEvent(element) {
+			element.addEventListener('focus',
+				function(ev) {
+					setTimeout(function() {
+						ev.target.selectionStart = ev.target.value.length
+						ev.target.selectionEnd = ev.target.value.length
+					})
+				})
 		}
 	})
